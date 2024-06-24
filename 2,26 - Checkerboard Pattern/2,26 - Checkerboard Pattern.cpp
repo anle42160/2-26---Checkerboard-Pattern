afade530f26ff40d082e112dc12946d547fcf01c
littl <littl@ANNA_LAPTOP>
2024-06-23 23:29:26 -0500

    Add project files.

---
+++ 2,26 - Checkerboard Pattern/2,26 - Checkerboard Pattern.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Defining string variables to reduce the amount of lines needed
+	string firstPattern = "\n* * * * * * * *";
+	string secondPattern = "\n * * * * * * * *";
+
+	//Pattern output
+	cout << firstPattern << secondPattern << firstPattern << secondPattern << firstPattern << secondPattern << firstPattern << secondPattern << endl;
+}
